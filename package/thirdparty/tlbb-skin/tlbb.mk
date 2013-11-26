7eb8ee1165980026f8dcf2d1aefd9274fc6ce5ae
luoj <luoj@geniatech.com>
2013-11-26 12:06:05 +0800

    init linux xbmc for amlogic mx

---
+++ package/thirdparty/tlbb-skin/tlbb.mk
@@ -0,0 +1,24 @@
+TLBB_VERSION = 35d8f2738c58a654ca98bea83c13d72707f00e04
+TLBB_SITE_METHOD = git
+TLBB_SITE = git@github.com:j1nx/tlbb.git
+TLBB_INSTALL_STAGING = YES
+TLBB_INSTALL_TARGET = YES
+TLBB_DEPENDENCIES = xbmc
+TEXTURE_PACKER=$(XBMC_DIR)/tools/TexturePacker/TexturePacker
+
+define TLBB_BUILD_CMDS
+	$(TEXTURE_PACKER) -use_none -i $(@D)/media -o $(@D)/media/Textures.xbt
+endef
+
+define TLBB_INSTALL_STAGING_CMDS
+        mkdir -p $(STAGING_DIR)/usr/share/xbmc/addons/skin.tlbb
+        cp -rf $(@D)/* $(STAGING_DIR)/usr/share/xbmc/addons/skin.tlbb/
+endef
+
+define TLBB_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/share/xbmc/addons/skin.tlbb/media
+	cd $(@D); cp -rf `ls -I media -1` $(TARGET_DIR)/usr/share/xbmc/addons/skin.tlbb
+	cp -f $(@D)/media/Textures.xbt $(TARGET_DIR)/usr/share/xbmc/addons/skin.tlbb/media
+endef
+
+$(eval $(call autotools-package))
